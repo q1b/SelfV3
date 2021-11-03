4c38e7c0cba041304d693e1b981cc9efe05936ee
sacarvy <sukhpreetben10@gmail.com>
2021-11-03 16:51:22 +0530

    just pusing dist folder to netlify

---
+++ dist/assets/inline-module-0f13ef75389bc5e586a17cf225f26180.b484b089.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./vendor.e4f2cdf9.js";const h=document.querySelectorAll("#annotate-highlight");h.forEach(o=>{const t=e(o,{type:"highlight",color:o.dataset.color});setTimeout(()=>{t.show()},1e3)});
